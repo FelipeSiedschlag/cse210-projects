8c94ef07dccaa300cfc7009495d6e169d2388ef2
FelipeSiedschlag <fsiedsch@byupathway.edu>
2025-03-06 13:13:29 -0300

    Completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // This code was used for parts 1 and 2 when the user needed to specify a magic number.
+        // Console.WriteLine("What is the magic number?");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        int attempts = 0;
+
+        while (number != guess)
+        {
+            attempts++;
+            Console.WriteLine("What is your guess?");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            Console.WriteLine($"Attempt #{attempts} made.");
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine($"Total number of attempts: {attempts}");
+
+
     }
 }
