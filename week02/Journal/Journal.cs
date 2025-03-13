164a8915e31d4ef68c6b7fca4dd44f54199ee246
FelipeSiedschlag <fsiedsch@byupathway.edu>
2025-03-13 17:28:36 -0300

    Starting Journal Project --> Created classes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,22 @@
+public class Journal
+{
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
+}
